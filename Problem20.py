83bb5940c4a31502b2114cf4bd2d95f905b177d2
Shivam Shukla <110848969+shivamim@users.noreply.github.com>
2024-02-29 20:08:14 +0530

    Add files via upload

---
+++ Problem20.py
@@ -0,0 +1,5 @@
+#TASK 
+''' Display three string “Name”, “Is”, “James” as “Name**Is**James”'''
+
+#SOLUTION 
+print("Name", "Is", "James", sep="**")
---
+++ Problem21.py
@@ -0,0 +1,8 @@
+#TASK 
+'''Convert Decimal number to octal using print() output formatting'''
+
+#SOLUTION 
+
+decimal_number = int(input("Enter your number:"))
+print("Decimal:", decimal_number)
+print("Octal:", "%o" % decimal_number)
